a87e7c50a664fb1e214dea4b9b531e863f199481
NONDweB <noemienino94@gmail.com>
2021-12-01 18:39:07 -0500

    modification css et html de index et des autres pages visibles du site + création de section et div dans index.html

--- monscript.js
+++ monscript.js
@@ -1,15 +1,1 @@
-
-
-/* page défilement */
-
-/* const gra = function(min, max) {
-  return Math.random() * (max - min) + min;
-}
-const init = function(){
-let items = document.querySelectorAll('section');
-for (let i = 0; i < items.length; i++){
-  items[i].style.background = randomColor({luminosity: 'light'});
-}
-cssScrollSnapPolyfill()
-}
-init(); */
+//Animation nav bar//
